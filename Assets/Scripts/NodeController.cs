2b069fa034733cf0b7cb68f1943623cdfa80793d
JoshuaStandridge <joshuastandridge@outlook.com>
2023-02-05 02:12:51 -0600

    Instant Game Over
    The script is fairly clean, just needs to start working.
    Also, changed neighbor node to a single item. Want to make sure it works with a single interaction, then will find some cheat way to have multiple neighbor nodes.

--- Assets/Scripts/NodeController.cs
+++ Assets/Scripts/NodeController.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 // script for Node Game Object
-
 // NOTE: IF THE NODE IS A BEGINNING NODE THERE IS NO PREVIOUS NODE
 // ALSO MAKE SURE THERE IS A COLLIDER ON NODES SO THEY CAN CLICK PROPERLY
 
 public class NodeController : MonoBehaviour
 {
-
-    // keeps track of what gameObjects are in our path
-    // so we can edit the root node's properties and
-    // show certain status of nodes and more
+    // keeps track of what gameObjects are in our path so we can edit the root node's properties and show certain status of nodes and more
 
     // old
     public NodeController nextNode;
@@ -23,8 +19,7 @@ public class NodeController : MonoBehaviour
     public RootNode myRoot;
 
     //public GameObject[] neighborNode;
-
-    public NodeController[] neighborNode;
+    public NodeController neighborNode;
 
     // connector
     /*
@@ -32,12 +27,12 @@ public class NodeController : MonoBehaviour
     */
 
     // flags 
-    public bool isFinalNode; // is this the final node in the sequence/path
     public bool isClaimed; // have we claimed this?
+    public bool isActive = false;
     public bool isBeginningNode; // is this one of the original nodes
+    public bool isEndNode; // is this the end node in the sequence/path
 
     // push and pull attributes
-
     public int curDistanceToClaim; // distance needed to claim the next node, if 0 we can claim
     public int maxDistanceToClaim; // if we want to claim this node, this is the max distance we have to traverse to get this node.
 
@@ -49,61 +44,75 @@ public class NodeController : MonoBehaviour
     public int addedPassive;
     public int addedActive;
 
-
     // timer variables
-
     protected float Timer;
-    public int delayAmount = 1; // 1 second 
+    public int delayAmount = 5; // 1 second 
 
-    // getters 
+    // scratch work
+    /*
+        if one is claimed before another 
+        if one is lost before another
+
+    O
+    if win: make self claimed/unclickable
+
+    O
+    if lose: become unclickable and set 
+
+    O
+    if lose: become clickable
+
+    */
+
+    // Mouse Button Down Function, activates everytime mouse cursor is pressed on collider of node
+    void OnMouseDown()
+    {
+        // SFX Click
+        // can't touch if not active: a neighbor node isn't claimed and it's not a beginning node
+        if (!isBeginningNode && neighborNode.isNodeClaimed())
+        {
+            return;
+        }
+
+
+        if (!isClaimed) // Active Growth
+        {
+            curDistanceToClaim -= myRoot.getActiveGrowth();
+        }
+
+        // claim
+        if (curDistanceToClaim <= 0 && !isClaimed)
+        { 
+            isClaimed = true; // change flag
+            // SFX Victory/Uprooted
+            currentNode.GetComponent<Renderer>().material.color = new(0, 255, 0); // claimed node turns green
+            myRoot.changeNumClaimed(1); // add 1 to number of nodes claimed
+            // add benefits to having node 
+            myRoot.changeActiveGrowth(addedActive);
+            myRoot.changePassiveGrowth(addedPassive);
+
+            // activate unclaimed neighbors
+            if (!neighborNode.isActive && !neighborNode.isClaimed)
+            {
+                neighborNode.isActive = true;
+            }
+        }
+    }
 
     public bool isNodeClaimed()
     {
         return isClaimed;
     }
 
-    // if neighboring node contested, do nothing
-    // if neighboring node captured, begin
-
-    // Mouse Button Down Function, activates everytime mouse cursor is pressed on collider of node
-    void OnMouseDown()
+    void resetDistance()
     {
-        // if previous node isn't claimed, we can't touch this one yet
-        // if it's a beginning node, we shouldn't be able
-        if (isBeginningNode || !neighborNode.isNodeClaimed())
-        {
-            return;
-        }
-
-        if (curDistanceToClaim <= 0 && !isClaimed)
-        { 
-            isClaimed = true; // change flag
-
-            currentNode.GetComponent<Renderer>().material.color = new(0, 255, 0); // claimed node turns green
-
-            if (nextNode != null)
-            {
-                nextNode.GetComponent<Renderer>().material.color = new(255, 255, 0); // if we have a nextNode turn it yellow
-            }
-
-            myRoot.changeNumClaimed(1); // add 1 to number of nodes claimed
-
-            // add benefits to having node 
-            myRoot.changeActiveGrowth(addedActive);
-            myRoot.changePassiveGrowth(addedPassive);
-
-
-        }
-
-        if (!isClaimed)
-        {
-            curDistanceToClaim -= myRoot.getActiveGrowth();
-        }
+        curDistanceToClaim = resetCurDistanceToClaim;
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        /*
         //For creating line renderer object
         lineRenderer = new GameObject("Line").AddComponent<LineRenderer>();
         //lineRenderer.startColor = Color.black;
@@ -116,21 +125,22 @@ public class NodeController : MonoBehaviour
         //For drawing line in the world space, provide the x,y,z values
         lineRenderer.SetPosition(0, new Vector3(x,y,z)); //x,y and z position of the starting point of the line
         lineRenderer.SetPosition(1, new Vector3(x,y,z)); //x,y and z position of the end point of the line
+        */
 
         // color stuff (initial)
-        if (isBeginningNode) // beginner node color (cyan)
+        if (isBeginningNode) // beginner node
         {
             currentNode.GetComponent<Renderer>().material.color = new(0, 255, 255);
         }
-        else if (isFinalNode) // final node color (red)
+        else if (isEndNode) // end node (red)
         {
             currentNode.GetComponent<Renderer>().material.color = new(255, 0, 0);
         }
-        else if (isNodeClaimed() && (isBeginningNode || previousNode.isNodeClaimed())) // claimed nodes, but not beginning node (green)
+        else if (isNodeClaimed() && (isBeginningNode || neighborNode.isNodeClaimed())) // claimed nodes, but not beginning node (green)
         {
             currentNode.GetComponent<Renderer>().material.color = new(0, 255, 0);
         }
-        else if (!isNodeClaimed() && previousNode.isNodeClaimed()) // clickable and not final boss node (yellow)
+        else if (!isNodeClaimed() && neighborNode.isNodeClaimed()) // clickable and not end boss node (yellow)
         {
             currentNode.GetComponent<Renderer>().material.color = new(255, 255, 0);
         }
@@ -143,55 +153,55 @@ public class NodeController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // if neighboring node contested, do nothing
+        // if neighboring node captured, begin
+
         // endpath claimed
-        if (isFinalNode && isNodeClaimed()) 
+        if (isEndNode && isNodeClaimed()) 
         {
             return;
         }
 
         // game over if we lose beginning node(s)
-        if (isBeginningNode && !isNodeClaimed()) 
+        if (isBeginningNode && isClaimed == false) 
         {
             SceneManager.LoadScene("GameOver");
         } 
 
-        if (!previousNode.isNodeClaimed())
+        if (!neighborNode.isNodeClaimed())
         {
             isClaimed = false;
+            isActive = false;
             return;
         }
 
-        // if the planet has pushed back all the way, we unclaim the previous node and have to reclaim the previous one to start getting this one
-        // again
-
+        // Non-click battle elements
         Timer += Time.deltaTime;
-
         if (Timer >= delayAmount)
         {
             Timer = 0f;
 
-            // if this node isn't claimed, and the previous one is
-            // there will be pushback and passive growth
-            if (!isNodeClaimed() && (previousNode.isNodeClaimed()))
+            // if this node isn't claimed, and the previous one is, there will be pushback and passive growth
+            if (!isClaimed && (!isActive))
             {
                 curDistanceToClaim -= myRoot.getPassiveGrowth();
                 curDistanceToClaim += pushBack;
             }
 
+            // lose 
             if (curDistanceToClaim >= maxDistanceToClaim)
             {
-                //previousNode.isClaimed = false;
+                // SFX Lose
                 neighborNode.isClaimed = false;
-
                 myRoot.numNodesClaimed--; // subtract a node claimed
 
-                //previousNode.curDistanceToClaim = previousNode.resetCurDistanceToClaim;
-                neighborNode.curDistanceToClaim = neighborNode.resetCurDistanceToClaim;
-                currentNode.curDistanceToClaim = currentNode.resetCurDistanceToClaim;
+                neighborNode.resetDistance();
+                currentNode.resetDistance();
 
-                if (currentNode.isFinalNode)
+                //s sprite change
+                if (currentNode.isEndNode)
                 {
-                    // change back to final Node color bc this should be inaccessble
+                    // change back to end Node color bc this should be inaccessble
                     currentNode.GetComponent<Renderer>().material.color = new(255, 0, 0);
                 }
                 else
@@ -199,8 +209,10 @@ public class NodeController : MonoBehaviour
                     currentNode.GetComponent<Renderer>().material.color = new(0, 0, 0); // should be inaccessible node color 
                 }
                 
+                // need to make neighbor unclaimed
                 //previousNode.GetComponent<Renderer>().material.color = new(255, 255, 0); // previous node should now be clickable
-
+                
+                // -- ! --
                 if (addedActive > 0)
                 {
                     myRoot.subtractActiveGrowth(previousNode.addedActive);
