9c3451a84ed7ec0de573c6d0fcc5a5579a0a7b7b
Eithan Austria <eithanmaustria00@gmail.com>
2023-02-09 17:04:14 -0600

    Fixed Merge Conflicts for Updates

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,55 +1,3 @@
-<<<<<<< Updated upstream
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameManager : MonoBehaviour
-{
-    // audio player
-    public AudioSource aS;
-    public AudioClip[] audioClipArray;
-
-    private static GameManager managerInstance;
-    
-    [SerializeField] private AudioSource _musicSource1, _musicSource2, _sfxClick, sfxCapture, _sfxDanger, _sfxOvertaken, _sfxVictory, _sfxDefeat;
-
-    public int currentSound;
-    public bool sfxTrigger = false;
-
-    void Awake()
-    {
-        DontDestroyOnLoad(this.gameObject);
-
-        // No duplicate managers
-        if (managerInstance == null) {
-            managerInstance = this;
-        } 
-        else {
-            Object.Destroy(gameObject);
-        }
-    }
-
-    // Start is called before the first frame update
-    void Update()
-    {
-        if (sfxTrigger == true)
-        {
-            PlaySound();
-        }
-    }
-
-    // Update is called once per frame
-
-    public void PlaySound(AudioClip clip)
-    {
-        //_sfxClick.PlayOneShot(clip);
-
-        aS.clip = audioClipArray[currentSound];
-        aS.PlayOneShot(aS.clip);
-        sfxTrigger = false;
-    }
-}
-=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -146,4 +94,3 @@ public class GameManager : MonoBehaviour
         }
     }
 }
->>>>>>> Stashed changes
--- Assets/Scripts/NodeStatUI.cs
+++ Assets/Scripts/NodeStatUI.cs
@@ -1,48 +1,3 @@
-<<<<<<< Updated upstream
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-
-public class NodeStatUI : MonoBehaviour
-{
-
-    // Text Property
-    public TMP_Text distanceNeededText;
-    public NodeController currentNode;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // intital distance needed, shows only if node is available to claim
-        //if (currentNode.previousNode.isNodeClaimed())
-        //{
-            //distanceNeededText.gameObject.SetActive(true);
-            distanceNeededText.text = currentNode.distance.ToString() + " / " + currentNode.maxDistance;
-        //}
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        // updates distance text with current distance of node
-        // only shows if node is available to claim
-
-        if (currentNode.previousNode.isNodeClaimed() && !currentNode.isNodeClaimed())
-        {
-            distanceNeededText.gameObject.SetActive(true);
-            distanceNeededText.text = currentNode.distance.ToString() + " / " + currentNode.maxDistance; ;
-        }
-        else
-        {
-            distanceNeededText.gameObject.SetActive(false);
-        }
-        
-    }
-}
-=======
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -101,4 +56,4 @@ public class NodeStatUI : MonoBehaviour
         
     }
 }
->>>>>>> Stashed changes
+
